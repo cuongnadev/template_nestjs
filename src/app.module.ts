ff5bda82b9dcd5fb13debe9c35e2fe399efce29b
Cậu Bé Java <154894942+cuongnadev@users.noreply.github.com>
2025-04-16 08:56:14 +0700

    update deploy #4

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,24 +6,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { ProductsModule } from './modules/products/products.module';
 import { Product } from './entities/product.entity';
+import { configDotenv } from 'dotenv';
+configDotenv()
 @Module({
   imports: [
     UserModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'root',
-      password: '040405@Nac',
-      database: 'template_nestjs',
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
       entities: [User, Product],
       synchronize: true,
       retryAttempts: 3,
       retryDelay: 3000,
+      ssl: true,
       extra: {
         connectionLimit: 10,
-        connectTimeout: 10000
-      }
+        connectTimeout: 10000,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      },
+      logging: true
     }),
     ProductsModule,
   ],
